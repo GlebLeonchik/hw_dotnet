1d5c7cafd166ca8432bd5ed2d3d3ef88d18347cd
GlebLeonchik <leowhite135@gmail.com>
2019-09-19 20:40:26 +0300

    #HW_01_Task1:created first project

---
+++ IIAcademyHomeWorks/HW_01_Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_01_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
