74b8177a1c85b051839a1886e366165b0cd79b3b
Gleb <GlebLeonchik@users.noreply.github.com>
2019-09-21 09:52:22 +0300

    Created hw_02_Task1

---
+++ BlankSolution/hw_02_Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace hw_02_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Hello";
+            int x = 30;
+            char symb = 'H';
+            Console.WriteLine(word + " " + x + " " + symb);
+            Console.WriteLine("----------------------");
+            var world = "World!";
+            var y = 30;
+            var Symb = 'F';
+            Console.WriteLine(world+" "+y+" "+Symb);
+            Console.WriteLine("----------------------");
+            string day = "Good Morning!";
+            object o = day;
+            object c = (string) o;
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+        }
+    }
+}
