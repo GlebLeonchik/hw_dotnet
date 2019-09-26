03443368e5f06e1d39eb9a1a005c9fbdb9cabd66
Gleb <GlebLeonchik@users.noreply.github.com>
2019-09-26 17:05:52 +0300

    Created HW_03_Task3

---
+++ HW_03_Task3/HW_03_Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HW_03_Task1
+{
+    class Program
+    {
+        static void calc()
+        {
+            Console.WriteLine("Введите первое слагаемое:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе слагаемое:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int amount = num1 + num2;
+            Console.WriteLine("Введите сумму этих слагаемых: ");
+            int check = Convert.ToInt32(Console.ReadLine());
+            if (check == amount)
+            {
+                Console.WriteLine("Правильно!");
+            }
+            else if(check!=amount&&check>amount)
+            {
+                Console.WriteLine("Неправильно, ожидается число поменьше!");
+            }
+            else if (check != amount && check < amount)
+            {
+                Console.WriteLine("Неправильно, ожидается число побольше!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            calc();
+            Console.ReadKey();
+        }
+    }
+}
