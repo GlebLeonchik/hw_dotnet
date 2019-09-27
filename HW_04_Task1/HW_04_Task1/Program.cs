ec74f7c4f7359bfd8de32a0845e701a101b7ec08
Gleb <GlebLeonchik@users.noreply.github.com>
2019-09-27 14:08:15 +0300

    Finished HW_04

---
+++ HW_04_Task1/HW_04_Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW_04_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i =90; i >= 65; i--)
+            {
+                char symb = Convert.ToChar(i);
+                Console.Write("| "+symb+" ");
+            }
+            Console.WriteLine("|");
+        }
+    }
+}
---
+++ HW_04_Task3/HW_04_Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HW_04_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = 1000, value = 0;
+            for (int i = 1; i <= 12; i++)
+            {
+                value = (money * 2) / 100 + value;
+                money += value;
+                value = Math.Truncate(value * 100) / 100;
+                money = Math.Truncate(money * 100) / 100;
+                if (i <= 10)
+                    Console.WriteLine("{0,0}",value);
+                if (i >= 3 && i <= 12)
+                    Console.WriteLine("{0,50}",money);
+                
+            }
+        }
+    }
+}
