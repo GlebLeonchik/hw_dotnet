20a249fa6011fd20fec5fa47f4620280f2204c6c
Gleb <GlebLeonchik@users.noreply.github.com>
2019-10-03 08:23:14 +0300

    Finished HW_05

---
+++ HW_05_Task1/HW_05_Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HW_05_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            int[] numRandom = new int[size];
+            int[] numInput = new int[size];
+            int[] numSumm = new int[size];
+            Random rdn = new Random();
+            for(int i = 0; i < numRandom.Length; i++)
+            {
+                numRandom[i] = rdn.Next(1, 11);
+            }
+            Console.WriteLine();
+            
+            for(int i = 0; i < numInput.Length; i++)
+            {
+                Console.WriteLine("Введите "+i+"-е число массива:");
+                numInput[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            for(int i = 0; i < numSumm.Length; i++)
+            {
+                numSumm[i] = numRandom[i] + numInput[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine("Random \tInput \tAmount");
+            for(int i = 0; i < size; i++)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}",numRandom[i],numInput[i],numSumm[i]);
+
+            }
+        }
+    }
+}
---
+++ HW_05_Task3/HW_05_Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HW_05_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+            Random rnd = new Random();
+            int[] array = new int[10];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 51);
+                Console.Write("| "+array[i]+" ");
+            }
+            Console.WriteLine("|");
+            Console.WriteLine();
+            for(int i = 0; i < array.Length-1; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (i < j)
+                    {
+                        value = array[i];
+                        array[i] = array[j];
+                        array[j] = value;
+                    }
+                    
+                }
+            }
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write("| "+array[i]+" ");
+            }
+            Console.WriteLine("|");
+        }
+
+    }
+}
