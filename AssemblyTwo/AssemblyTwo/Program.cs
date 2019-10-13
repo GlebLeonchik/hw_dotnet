4a55fe6f3278b20d6c8fa5c78c61ea1ed0402cc3
Gleb <GlebLeonchik@users.noreply.github.com>
2019-10-13 15:15:09 +0300

    Finished HW_07

---
+++ AssemblyTwo/AssemblyTwo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ReSeller : AutomobileAudiBase
+{
+}
+
+namespace AssemblyTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Base Info:");
+            ReSeller ReSell = new ReSeller();
+            ReSell.Info();
+            Console.WriteLine();
+            Console.WriteLine("ReSell Info:");
+            ReSell.GetVIN("3333 - 3333 - 3333 - 3333");
+            ReSell.Color = "Yellow";
+            ReSell.Info();
+            Console.ReadKey();
+        }
+    }
+}
